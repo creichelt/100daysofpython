1c29f3f585f6ed6d5b4a4b8b338db29cb00b488e
Clemens Reichelt <creichelt@me.com>
2021-02-25 03:35:49 +0100

    adding project 10: calculator

---
+++ 010_calculator.py
@@ -0,0 +1,56 @@
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------.
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------'
+|_____________________|
+"""
+
+def add(n1,n2):
+    return n1 + n2
+
+def subtract(n1,n2):
+    return n1 - n2
+
+def multiply(n1,n2):
+    return n1 * n2
+
+def divide(n1,n2):
+    return n1 / n2
+
+def calculator():
+    print(logo)
+    num1 = float(input('\nEnter number: '))
+    keep_going = True
+    while going:
+        operand = input("Pick an operation (+, -, *, /):  ")
+        num2 = float(input('\nEnter number: '))
+        calc_func = operations[operand]
+        answer = calc_func(num1,num2)
+        print(f"{num1} {operand} {num2} = {answer}")
+        continued = input(f"Continue with result {answer}? Type: y. Continue with new calculation? Type: n. Quit? Type: q. ")
+        if continued == 'y':
+            num1 = answer
+        elif continued == 'n':
+            keep_going = False
+            calculator()
+        elif continued == 'q':
+            break
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+    }
+
+calculator()
